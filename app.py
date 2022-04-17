a9f7b3c4479a960b0a38e2ae006ba6440864b77c
migdracios <migdracios@gmail.com>
2022-04-17 23:28:33 +0900

    tk를 활용한 숫자 입력 버튼

--- app.py
+++ app.py
@@ -1,5 +1,31 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.title('계산기')
-window.mainloop()
+
+win = tk.Tk()
+win.title('계산기')
+
+
+
+tk.Button(win, text='1', width=10, height=5).grid(column=0, row=1)
+tk.Button(win, text='2', width=10, height=5).grid(column=1, row=1)
+tk.Button(win, text='3', width=10, height=5).grid(column=2, row=1)
+tk.Button(win, text='4', width=10, height=5).grid(column=3, row=1)
+
+tk.Button(win, text='5', width=10, height=5).grid(column=0, row=2)
+tk.Button(win, text='6', width=10, height=5).grid(column=1, row=2)
+tk.Button(win, text='7', width=10, height=5).grid(column=2, row=2)
+tk.Button(win, text='8', width=10, height=5).grid(column=3, row=2)
+
+tk.Button(win, text='9', width=10, height=5).grid(column=0, row=3)
+tk.Button(win, text='0', width=10, height=5).grid(column=1, row=3)
+tk.Button(win, text='+', width=10, height=5).grid(column=2, row=3)
+tk.Button(win, text='-', width=10, height=5).grid(column=3, row=3)
+
+tk.Button(win, text='/', width=10, height=5).grid(column=0, row=4)
+tk.Button(win, text='*', width=10, height=5).grid(column=1, row=4)
+tk.Button(win, text='C', width=10, height=5).grid(column=2, row=4)
+tk.Button(win, text='=', width=10, height=5).grid(column=3, row=4)
+
+
+
+win.mainloop()
