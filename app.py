1efae781b5d6f4eab68c127babd28b6378a95f00
migdracios <migdracios@gmail.com>
2022-04-17 23:34:48 +0900

    에디터 초깃값, 위치 및 크기 조정

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import tkinter as tk
 win = tk.Tk()
 win.title('계산기')
 
-
+disValue = 0                                                        # 초기값을 0으로 변수 초기화
+str_value = tk.StringVar()                                          # 문자열로 표시
+str_value.set(str(disValue))                                        # 초깃값을 문자열로 변환
+dis = tk.Entry(win, textvariable=str_value, justify="right")        # 에디터 엔트리 형식에 넣기
+dis.grid(column=0, row=0, columnspan= 4, ipadx=80, ipady=30)        # 에디터 위치, 크기 조정
 
 tk.Button(win, text='1', width=10, height=5).grid(column=0, row=1)
 tk.Button(win, text='2', width=10, height=5).grid(column=1, row=1)
